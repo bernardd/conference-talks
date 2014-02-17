8c0ebbba89c7bbc7d2f59466bbbf39a06b8fca03
Bernard Duggan <bduggan@shoretel.com>
2014-02-17 16:40:19 +1100

    Add SneakingErlangIn talk

---
+++ SneakingErlangIn/slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Sneaking Erlang in<br>Through the Back Door',
+    subtitle: 'Subtitle Goes Here',
+    eventInfo: {
+      title: 'Erlang Factory SF',
+      date: '2013-03-07'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: false, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Bernard Duggan',
+    company: 'Sr. Telephony Engineer<br>ShoreTel',
+    //gplus: 'http://plus.google.com/1234567890',
+    //twitter: '@yourhandle',
+    www: 'http://shoretel.com',
+    github: 'http://github.com/ShoreTel-Inc'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
