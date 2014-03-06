c0d43dcc9576cc7f3742f6d005dcc48df9b4c7fe
Bernard Duggan <bduggan@shoretel.com>
2014-03-05 22:21:05 -0800

    Add lightning talk

---
+++ erldLightning/slide_config.js
@@ -0,0 +1,41 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'erld: You need it.',
+    subtitle: 'no, relaly, you do.',
+    eventInfo: {
+      title: 'Erlang Factory SF',
+      date: '2014-03-06'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: false, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Bernard Duggan',
+    company: 'Sr. Telephony Engineer<br>ShoreTel',
+    //gplus: 'http://plus.google.com/1234567890',
+    //twitter: '@yourhandle',
+    www: 'http://shoretel.com',
+    github: 'http://github.com/bernardd',
+    email: 'bduggan@shoretel.com'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
