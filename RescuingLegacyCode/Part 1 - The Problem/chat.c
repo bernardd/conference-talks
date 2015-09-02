5f01edafd67b7297bf3385e938a006612308879a
Bernard Duggan <bduggan@shoretel.com>
2015-09-02 14:29:11 +1000

    WIP

--- RescuingLegacyCode/Part 1 - The Problem/chat.c
+++ RescuingLegacyCode/Part 1 - The Problem/chat.c
@@ -2,17 +2,30 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <netdb.h>
 #include <string.h>
 
-#define LD(m) log(1, debug, false, m);
-#define LI(m) log(1, info, false, m);
+#include "chat.h"
+
+#define LD(m) logmsg(1, debug, 0, m)
+#define LI(m) logmsg(1, info, 0, m)
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 int logger_sock;
-char [512][512] users;
+char users[512][512];
 int nusers = 0;
+fd_set fds;
+int max_fd = 0;
+
+void setup_logger();
+void read_data();
+void logmsg(int level, type t, unsigned char syslog, char *m);
+void run_server();
 
 int main(void)
 {
+	FD_ZERO(&fds);
 	setup_logger();
 	read_data();
 
@@ -22,39 +35,49 @@ int main(void)
 void setup_logger()
 {
 	if (!fork())
-		exec("logger");
+		execl("logger", "%s", "logger");
 
 	logger_sock = socket(AF_INET, SOCK_STREAM, 0);
 	struct hostent *server = gethostbyname("localhost");
 	struct sockaddr_in addr = {};
-	bcopy((char*)server->h_addr, (char*)&addr.sin_addr.saddr, server->h_length);
+	bcopy((char*)server->h_addr, (char*)&addr.sin_addr.s_addr, server->h_length);
 	addr.sin_port = htons(LOGGER_PORT);
-	while (connect(sock, (struct sockaddr*)&addr, sizeof(addr))) {}
+	while (connect(logger_sock, (struct sockaddr*)&addr, sizeof(addr))) {}
 }
 
 void read_data()
 {
 	LI("Opening data file");
 	FILE *f = fopen("data.txt", "r");
-	while (fgets(users[nusers++], sizeof(users[0]), f) != EOF) {}
+	while (fgets(users[nusers++], sizeof(users[0]), f) != NULL) {}
 	nusers--;
 	fclose(f);
 }
 
-void log(int level, type t, bool syslog, char *m)
+void logmsg(int level, type t, unsigned char syslog, char *m)
 {
-	log_message m = {};
-	m.level = level;
-	m.type = type;
-	m.syslog = syslog;
-	strncpy(m.message, m, sizeof(m.message)-1);
+	log_message msg = {};
+	msg.level = level;
+	msg.t = t;
+	msg.syslog = syslog;
+	strncpy(msg.message, m, sizeof(msg.message)-1);
 }
 
 void run_server()
 {
 	int server_sock = socket(AF_INET, SOCK_STREAM, 0);
-	sockaddr_in addr = {};
+	struct sockaddr_in addr = {};
 	addr.sin_port = htons(SERVER_PORT);
-	bind(sock, (struct sockaddr*)&addr, sizeof(addr));
+	bind(server_sock, (struct sockaddr*)&addr, sizeof(addr));
+	listen(server_sock, 5);
+	FD_ADD(server_sock, &fds);
 
+	while (1) {
+		select(server_sock+1, &fds, NULL, NULL, NULL);
+		struct sockaddr new_addr;
+		socklen_t len;
+		int new_sock = accep(server_sock, &new_addr, &len);
+		dump_data(new_sock);
+		close(new_sock);
+	}
 }
--- RescuingLegacyCode/Part 1 - The Problem/chat.h
+++ RescuingLegacyCode/Part 1 - The Problem/chat.h
@@ -2,19 +2,19 @@
 #define _CHAT_H_
 
 #define LOGGER_PORT 9999
-#define LOGGER_PORT 8888
+#define SERVER_PORT 8888
 
-enum type {
+typedef enum {
 	debug,
 	info,
 	warning,
 	error
-};
+} type;
 
 typedef struct {
 	int level;
 	type t;
-	bool syslog;
+	unsigned char syslog;
 	char message[1024];
 } log_message;
 
